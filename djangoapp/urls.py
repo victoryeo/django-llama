7856a3c879a8b72daf8245633986f9c5fdb4e14b
yeosiowvic <victoryeo1974@gmail.com>
2024-06-26 09:14:37 +0800

    update comment

--- djangoapp/urls.py
+++ djangoapp/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('db_status/', views.db_status, name='db_status'),  # endpoint to check build status
-    path('build_db/', views.build_db, name='build_db'),  # New endpoint to trigger DB building
+    path('build_db/', views.build_db, name='build_db'),  # endpoint to trigger DB building
    ]
