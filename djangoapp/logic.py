65f6042224d510718a53051603c884b9daee39e5
yeosiowvic <victoryeo1974@gmail.com>
2024-06-26 10:09:50 +0800

    fix LLMChain db, fix model_path

--- djangoapp/logic.py
+++ djangoapp/logic.py
@@ -131,7 +131,7 @@ def answer_query(query):
     )
     # Load the LlamaCpp language model, adjust GPU usage based on your hardware
     llm = LlamaCpp(
-        model_path="models/llama-2-7b-chat.Q4_0.gguf",
+        model_path="djangoapp/models/llama-2-7b-chat.Q4_0.gguf",
         n_gpu_layers=40,
         n_batch=512,  # Batch size for model processing
         verbose=False,  # Enable detailed logging for debugging
@@ -145,7 +145,7 @@ def answer_query(query):
     """
     prompt = PromptTemplate(template=template, input_variables=["question"])
     
-    chain = LLMChain(prompt=prompt, llm=llm, db=db)
+    chain = LLMChain(prompt=prompt, llm=llm)
     
     result = chain({"question": query}, return_only_outputs=True)
 
