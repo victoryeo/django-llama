3392aedfa680edfa7d33313a0a8ebdc01cdc2f37
yeosiowvic <victoryeo1974@gmail.com>
2024-07-20 13:19:35 +0800

    mod categories

--- nextjs-frontend/src/libs/ui/component/categories.tsx
+++ nextjs-frontend/src/libs/ui/component/categories.tsx
@@ -10,30 +10,38 @@ export const Categories = () => {
   const pathname = usePathname()
 
   return (
-    <div className="grid grid-rows-2 md:grid-cols-4 gap-4">
-      <div style={{ backgroundColor: "#ddd"}}>Design
+    <div className="grid grid-rows-2 md:grid-cols-4 gap-4 font-semibold">
+      <div style={{ backgroundColor: "#ddd"}}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt=""/>
+        Design
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>Development
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
+        Development
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>Marketing
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" alt=""/>
+        Marketing
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>IT and Software
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt=""/>
+        IT and Software
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>Personal Development
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt=""/>
+        Personal Development
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>Business
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" alt=""/>
+        Business
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>Music
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" alt=""/>
+        Music
       </div>
-      <div style={{ backgroundColor: "#ddd" }}>Photography
+      <div style={{ backgroundColor: "#ddd" }}>
         <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" alt=""/>
+        Photography
       </div>
     </div>
   )
