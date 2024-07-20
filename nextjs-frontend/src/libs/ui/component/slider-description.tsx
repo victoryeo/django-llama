2ab251b7d31cfb1e8033be921dcf245cf6426d74
yeosiowvic <victoryeo1974@gmail.com>
2024-07-20 21:26:43 +0800

    add carousel slider

---
+++ nextjs-frontend/src/libs/ui/component/slider-description.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { images } from "../../utils/carousel/constants";
+import left from "../../../../public/left.svg";
+import right from "../../../../public/right.svg";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+type Props = {
+  activeImage: any;
+  clickNext: any;
+  clickPrev: any;
+};
+
+const Description = ({ activeImage, clickNext, clickPrev }: Props) => {
+  return (
+    <div className="grid place-items-start w-full bg-[#e7dfd9] relative md:rounded-tr-3xl md:rounded-br-3xl">
+      <div className="uppercase text-sm absolute right-4 top-2 underline-offset-4 underline">
+        
+      </div>
+      {images.map((elem, idx) => (
+        <div
+          key={idx}
+          className={`${idx === activeImage
+              ? "block w-full h-full md:h-[80vh] py-10 md:px-20 px-10 text-left"
+              : "hidden"
+            }`}
+        >
+          
+          <div className="py-16 text-5xl font-extrabold">
+            {elem.title}
+          </div>
+          <div className="leading-relaxed font-medium text-base tracking-wide h-60 md:h-40 italic text-gray-600">
+            {" "}
+            {elem.desc}
+          </div>
+
+          <button className="bg-[#ecae7e] text-white uppercase px-4 py-2 rounded-md my-10">
+            order now
+          </button>
+          <div className="absolute md:bottom-1 bottom-10 right-10 md:right-0 w-full flex justify-center items-center">
+            <div
+              className="absolute bottom-2 right-10 cursor-pointer"
+              onClick={clickPrev}
+            >
+              <Image src={left} alt="" />
+            </div>
+
+            <div
+              className="absolute bottom-2 right-2 cursor-pointer"
+              onClick={clickNext}
+            >
+              <Image src={right} alt="" />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Description;
---
+++ nextjs-frontend/src/libs/ui/component/slider.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+import { images } from "../../utils/carousel/constants";
+import Description from "./slider-description";
+
+const Slider = () => {
+  const [activeImage, setActiveImage] = useState(0);
+
+  const clickNext = () => {
+    activeImage === images.length - 1
+      ? setActiveImage(0)
+      : setActiveImage(activeImage + 1);
+  };
+  const clickPrev = () => {
+    activeImage === 0
+      ? setActiveImage(images.length - 1)
+      : setActiveImage(activeImage - 1);
+  };
+
+  return (
+    <div className="grid place-items-center md:grid-cols-2 grid-cols-1 w-full mx-auto max-w-8xl px-4"
+      style={{ backgroundColor: "#fff" }}>
+      <div
+        className={`w-full flex justify-center items-center gap-4 transition-transform ease-in-out duration-500 md:rounded-2xl p-6 md:p-0`}
+      >
+        {images.map((elem, idx) => (
+          <div
+            key={idx}
+            className={`${idx === activeImage
+                ? "block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out"
+                : "hidden"
+              }`}
+          >
+            <Image
+              src={elem.src}
+              alt=""
+              width={400}
+              height={400}
+              className="w-full h-full object-cover md:rounded-tl-3xl md:rounded-bl-3xl"
+            />
+          </div>
+        ))}
+      </div>
+      <Description
+        activeImage={activeImage}
+        clickNext={clickNext}
+        clickPrev={clickPrev}
+      />
+    </div>
+  );
+};
+
+export default Slider;
