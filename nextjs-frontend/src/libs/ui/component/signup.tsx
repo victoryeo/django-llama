3a31875132a99529775db4f50fa62da9b0d9e5d2
victoryeo <victoryeo1974@gmail.com>
2024-07-26 21:36:27 +0800

    add sign up content

--- nextjs-frontend/src/libs/ui/component/signup.tsx
+++ nextjs-frontend/src/libs/ui/component/signup.tsx
@@ -10,6 +10,7 @@ export const Signup = () => {
   const pathname = usePathname()
 
   return (
+    <form>
     <div className="grid grid-rows-2 md:grid-cols-2 gap-y-4 gap-x-4 px-40"
       style={{ backgroundColor: "#ffffff" }}>
         <div>
@@ -19,11 +20,39 @@ export const Signup = () => {
         </a>
       </div>
       <div>
-        <a href='/'>
-          
-        Sign up
-        </a>
+        
+        <div className="grid grid-rows-6 md:grid-cols-1 gap-y-4 gap-x-4 px-40"
+        style={{ backgroundColor: "#ffffff" }}>  
+            <div className="bg-transparent text-black-700 font-semibold text-xl">
+                Sign up and start learning
+            </div>
+            <div>
+                <label for="full_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Full name" required />
+            </div>
+            <div>
+                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required />
+            </div>
+            <div>
+                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="•••••••••" required />
+            </div>
+            <div class="flex items-start mb-6">
+                <div class="flex items-center h-5">
+                <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required />
+                </div>
+                <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
+            </div>
+            
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
+            
+            <div>
+                Already have an account? Login in
+            </div>
+        </div>
       </div>
     </div>
+    </form>
   )
 }
