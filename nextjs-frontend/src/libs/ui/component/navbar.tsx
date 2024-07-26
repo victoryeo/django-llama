85334659c038963fd5ee4aba1471a1eb74dbd3af
victoryeo <victoryeo1974@gmail.com>
2024-07-26 20:18:48 +0800

    add sign up button

--- nextjs-frontend/src/libs/ui/component/navbar.tsx
+++ nextjs-frontend/src/libs/ui/component/navbar.tsx
@@ -51,6 +51,12 @@ export const Navbar = () => {
 
       <div className="nav-right">
       <button   
+        className="mr-2 bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        onClick={() => router.push('/signup')}
+      >
+        Sign-up
+      </button>
+      <button   
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         onClick={() => router.push('/login')}
       >
