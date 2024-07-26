2f1f9b2070ee440f075cb05b5cbf90e816bc69d5
victoryeo <victoryeo1974@gmail.com>
2024-07-26 21:41:32 +0800

    add login link

--- nextjs-frontend/src/libs/ui/component/signup.tsx
+++ nextjs-frontend/src/libs/ui/component/signup.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import Image from 'next/image';
 import { useRouter, usePathname } from 'next/navigation';
-
+import Link from 'next/link'
 
 export const Signup = () => {
   const router = useRouter()
@@ -48,7 +48,8 @@ export const Signup = () => {
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
             
             <div>
-                Already have an account? Login in
+                Already have an account? 
+                <Link class="underline px-2" href="/login">Log in</Link>
             </div>
         </div>
       </div>
