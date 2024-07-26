cdd523e7f3e925b267df5183678dadef205b73c7
victoryeo <victoryeo1974@gmail.com>
2024-07-26 20:51:05 +0800

    add login component

---
+++ nextjs-frontend/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Login } from "../../libs/ui/component/login";
+import { Navbar } from "../../libs/ui/component/navbar";
+
+export default function Page() {
+
+  return (
+    <>
+       <div className="">
+        <Navbar />
+        <Login/>
+        </div>
+    </>
+  );
+}
---
+++ nextjs-frontend/src/libs/ui/component/login.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React from 'react';
+import Image from 'next/image';
+import { useRouter, usePathname } from 'next/navigation';
+
+
+export const Login = () => {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  return (
+    <div className="grid grid-rows-2 md:grid-cols-2 gap-y-4 gap-x-4 px-40"
+      style={{ backgroundColor: "#ffffff" }}>
+        <div>
+        <a href='/'>
+          <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" alt=""/>
+
+        </a>
+      </div>
+      <div>
+        <a href='/'>
+          
+        Login
+        </a>
+      </div>
+    </div>
+  )
+}
