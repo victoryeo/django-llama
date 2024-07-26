70d010a710866509652b7a3fbb50a94ca473513c
victoryeo <victoryeo1974@gmail.com>
2024-07-26 20:45:25 +0800

    add signup component

---
+++ nextjs-frontend/src/app/signup/page.tsx
@@ -0,0 +1,14 @@
+import { Signup } from "../../libs/ui/component/signup";
+import { Navbar } from "../../libs/ui/component/navbar";
+
+export default function Page() {
+
+  return (
+    <>
+       <div className="">
+        <Navbar />
+        <Signup/>
+        </div>
+    </>
+  );
+}
