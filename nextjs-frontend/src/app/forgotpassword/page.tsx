ab79f2f994d9339ec2d5a965a536decfd40e1462
victoryeo <victoryeo1974@gmail.com>
2024-07-28 12:57:20 +0800

    add forgot password page

---
+++ nextjs-frontend/src/app/forgotpassword/page.tsx
@@ -0,0 +1,13 @@
+import { Navbar } from "../../libs/ui/component/navbar";
+
+export default function Page() {
+
+  return (
+    <>
+       <div className="">
+        <Navbar />
+        
+        </div>
+    </>
+  );
+}
