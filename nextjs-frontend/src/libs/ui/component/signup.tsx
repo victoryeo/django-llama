14c99afc7d625879c6d31cb815c481bbd9410bef
victoryeo <victoryeo1974@gmail.com>
2024-07-29 15:01:18 +0800

    add signup handling

--- nextjs-frontend/src/libs/ui/component/signup.tsx
+++ nextjs-frontend/src/libs/ui/component/signup.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link'
@@ -8,6 +8,12 @@ import Link from 'next/link'
 export const Signup = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const [fullname, setFullname] = useState('')
+
+  const handleSignup = async (e: any) => {
+    e.preventDefault() // prevents page reload
+    alert(`${fullname} have signed up!`)
+  }
 
   return (
     <form>
@@ -28,7 +34,10 @@ export const Signup = () => {
             </div>
             <div>
                 <label for="full_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First name</label>
-                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Full name" required />
+                <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Full name" 
+                value={fullname}
+                onChange={e=> setFullname(e.target.value)}
+                required />
             </div>
             <div>
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
@@ -45,7 +54,9 @@ export const Signup = () => {
                 <label for="remember" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a>.</label>
             </div>
             
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type='submit'
+            onClick={handleSignup}>Sign up</button>
             
             <div>
                 Already have an account? 
