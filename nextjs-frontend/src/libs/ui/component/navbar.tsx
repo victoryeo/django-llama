1bfb79a1c450ce6f341a348a085819fd5ae98130
victoryeo <victoryeo1974@gmail.com>
2024-08-01 10:46:42 +0800

    fix stroke linecap warning

--- nextjs-frontend/src/libs/ui/component/navbar.tsx
+++ nextjs-frontend/src/libs/ui/component/navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => {
         </a>
         <span className="bg-transparent text-blue-700 font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded"></span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
         </svg>
         <div className="hidden w-full md:block md:w-auto flex items-center" id="navbar-default">
           <ul className="flex items-center font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
