a7212daeb55cac11a74ae9f91b503c62f6d84d5e
victoryeo <victoryeo1974@gmail.com>
2024-08-02 12:45:15 +0800

    remove extra type submit

--- nextjs-frontend/src/libs/ui/component/login.tsx
+++ nextjs-frontend/src/libs/ui/component/login.tsx
@@ -60,7 +60,7 @@ export const Login = () => {
             <div className="bg-transparent text-black-700 font-semibold text-xl">
                 Login to continue learning
             </div>
-            
+          
             <div>
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                 <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" 
@@ -75,8 +75,7 @@ export const Login = () => {
             <div></div>
             
             
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            type='submit'
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"            
             onClick={handleLogin}>Log in</button>
             
             <div>
