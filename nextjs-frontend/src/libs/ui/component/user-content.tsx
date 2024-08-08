fc7126f3c37ce9bcaae77582bb5a7de176e3060d
victoryeo <victoryeo1974@gmail.com>
2024-08-08 20:57:08 +0800

    add video playback

--- nextjs-frontend/src/libs/ui/component/user-content.tsx
+++ nextjs-frontend/src/libs/ui/component/user-content.tsx
@@ -1,13 +1,18 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter, usePathname } from 'next/navigation';
-
+import ReactPlayer from 'react-player'
 
 export const UserContent = () => {
   const router = useRouter()
   const pathname = usePathname()
+  const [video, setVideo] = useState<any>(null)
+  useEffect(()=>{
+    setVideo(<ReactPlayer url="https://www.youtube.com/watch?v=gfU1iZnjRZM" 
+      controls={true} width="120%" height="auto"/>)
+  }, [])
 
   return (
     <div className="grid grid-rows-3 md:grid-cols-5 gap-y-4 px-14"
@@ -37,6 +42,9 @@ export const UserContent = () => {
       <div className='lowerFeature'>
       </div>
       <div className='lowerFeature'>
+      </div>
+      <div className='player-wrapper '>
+      {video}
       </div>
       <div className='lowerFeature'>
       </div>
