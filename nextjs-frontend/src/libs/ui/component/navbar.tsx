ef29ba2ffcde2fa02b6c4dcd8dd6da2ffe636a8b
victoryeo <victoryeo1974@gmail.com>
2024-08-09 11:25:29 +0800

    add dropdown menu

--- nextjs-frontend/src/libs/ui/component/navbar.tsx
+++ nextjs-frontend/src/libs/ui/component/navbar.tsx
@@ -6,11 +6,12 @@ import { useRouter, usePathname } from 'next/navigation';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { signOut } from 'next-auth/react';
 import Search from './search';
-//import { LoginContext, LoginContextType } from './login';
 import { useLoginContext } from '@/libs/ui/providers/LoginContext'
 import { useSession } from "next-auth/react"
 import { User } from "@/libs/auth.config"
 import { Session } from 'next-auth';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 
 type Props = {
   session : Session | null;
@@ -37,6 +38,7 @@ export const Navbar = ({session} : Props) => {
 
   const handleProfile = () => {
     console.log("handleProfile")
+
   }
 
   return (
@@ -103,9 +105,47 @@ export const Navbar = ({session} : Props) => {
 
       <span className="bg-transparent text-blue-700 font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded"></span>
 
-      { (isLogin || session) && <a onClick={() => handleProfile()}>
-        <img src="/info.svg" alt="login" />
-      </a>
+      { (isLogin || session) && 
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+            <img src="/info.svg" alt="login" />
+            <ChevronDownIcon aria-hidden="true" className="-mr-1 h-5 w-5 text-gray-400" />
+          </MenuButton>
+        </div>
+
+        <MenuItems
+          transition
+          className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+        >
+          <div className="py-1">
+            <MenuItem>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              >
+                Account settings
+              </a>
+            </MenuItem>
+            <MenuItem>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              >
+                Support
+              </a>
+            </MenuItem>
+            <MenuItem>
+              <a
+                href="#"
+                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              >
+                License
+              </a>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </Menu>
       }
       </div>
     </nav>
