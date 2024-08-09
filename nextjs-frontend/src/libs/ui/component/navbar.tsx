38e4aa87445c4164b599a879000275df4e3638ec
victoryeo <victoryeo1974@gmail.com>
2024-08-09 11:42:48 +0800

    add profile dialog box

--- nextjs-frontend/src/libs/ui/component/navbar.tsx
+++ nextjs-frontend/src/libs/ui/component/navbar.tsx
@@ -1,10 +1,9 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
 import Image from 'next/image';
 import { useRouter, usePathname } from 'next/navigation';
 import Dropdown from 'react-bootstrap/Dropdown';
-import { signOut } from 'next-auth/react';
 import Search from './search';
 import { useLoginContext } from '@/libs/ui/providers/LoginContext'
 import { useSession } from "next-auth/react"
@@ -12,6 +11,7 @@ import { User } from "@/libs/auth.config"
 import { Session } from 'next-auth';
 import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
 
 type Props = {
   session : Session | null;
@@ -22,6 +22,8 @@ export const Navbar = ({session} : Props) => {
   const pathname = usePathname()
   const { data: sessionclient, update } = useSession()
   const [user, setUser] = useState<User>({} as User)
+  const [open, setOpen] = useState(true)
+
   console.log("sessionme", session);
   console.log("sessionclient", sessionclient)
 
@@ -39,6 +41,12 @@ export const Navbar = ({session} : Props) => {
   const handleProfile = () => {
     console.log("handleProfile")
 
+  }
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log("onSubmit", open)
+    setOpen(true)
   }
 
   return (
@@ -119,14 +127,16 @@ export const Navbar = ({session} : Props) => {
           className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
         >
           <div className="py-1">
+            <form onSubmit={onSubmit}>
             <MenuItem>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              <button
+                type="submit"
+                className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
               >
                 Account settings
-              </a>
+              </button>
             </MenuItem>
+            </form>
             <MenuItem>
               <a
                 href="#"
@@ -148,6 +158,62 @@ export const Navbar = ({session} : Props) => {
       </Menu>
       }
       </div>
+
+  <Dialog open={open} onClose={setOpen} className="relative z-10">
+    <DialogBackdrop
+      transition
+      className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+    />
+
+    <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+      <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <DialogPanel
+          transition
+          className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+        >
+          <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                
+              </div>
+              <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                <DialogTitle as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                  Profile
+                </DialogTitle>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    You are {session?.user?.name}
+                  </p>
+                </div>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Your email is {session?.user?.email}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+            <button
+              type="button"
+              onClick={() => setOpen(false)}
+              className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+            >
+              Okay
+            </button>
+            <button
+              type="button"
+              data-autofocus
+              onClick={() => setOpen(false)}
+              className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+            >
+              Cancel
+            </button>
+          </div>
+        </DialogPanel>
+      </div>
+    </div>
+  </Dialog>
     </nav>
   );
 };
