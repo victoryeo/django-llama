a7f68fce4095b0ac394f557050c9f9bbec8e6a3c
victoryeo <victoryeo1974@gmail.com>
2024-10-20 23:00:23 +0800

    bg white

--- nextjs-frontend/src/libs/ui/component/sidebar-desktop.tsx
+++ nextjs-frontend/src/libs/ui/component/sidebar-desktop.tsx
@@ -19,7 +19,7 @@ export function SidebarDesktop(props: SidebarDesktopProps) {
 
   return (
     <aside className='w-[200px] max-w-xs h-screen fixed left-0 top-0 z-40 border-r'>
-      <div className='h-full px-3 py-4'>
+      <div className='bg-white h-full px-3 py-4'>
         <h3 className='mx-3 text-lg font-semibold text-foreground'>Sidebar</h3>
         <div className='mt-9'>
           <div className='flex flex-col gap-4 w-full  items-start'>
